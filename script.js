2f44e33b3bcca01adcf79f812cfeab6862a8caf2
izyazev <39080024+izyazev@users.noreply.github.com>
2023-04-05 19:08:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.addEventListener("click", function() {
+  scrollToTop();
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
