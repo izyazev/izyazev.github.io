0939d2dc338b98731f331abb53dfce6023d6dd7a
izyazev <39080024+izyazev@users.noreply.github.com>
2023-04-09 17:38:54 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,55 @@ function scrollToTop() {
     behavior: "smooth"
   });
 }
+
+/* Конец основного кода страницы */
+
+const galleryImages = document.querySelectorAll('.gallery-images img');
+const prevButton = document.querySelector('.gallery-button-prev');
+const nextButton = document.querySelector('.gallery-button-next');
+let activeImageIndex = 0;
+
+function showImage(index) {
+  galleryImages.forEach((image) => {
+    image.classList.remove('active');
+  });
+  galleryImages[index].classList.add('active');
+}
+
+function nextImage() {
+  activeImageIndex++;
+  if (activeImageIndex >= galleryImages.length) {
+    activeImageIndex = 0;
+  }
+  showImage(activeImageIndex);
+}
+
+function prevImage() {
+  activeImageIndex--;
+  if (activeImageIndex < 0) {
+    activeImageIndex = galleryImages.length - 1;
+  }
+  showImage(activeImageIndex);
+}
+
+let intervalId = setInterval(() => {
+  nextImage();
+}, 5000);
+
+prevButton.addEventListener('click', () => {
+  prevImage();
+  clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    nextImage();
+  }, 5000);
+});
+
+nextButton.addEventListener('click', () => {
+  nextImage();
+  clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    nextImage();
+  }, 5000);
+});
+
+showImage(activeImageIndex);
